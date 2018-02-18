a647129463ed6691433437d43ee83627c49644bf
Maggie Ruimin Zhao <rz2390@columbia.edu>
2018-02-18 18:44:02 -0500

    Add files via upload

---
+++ 261_Graph_Valid_Tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res=[]
+        self.helper(root,res,0)
+        return res
+    def helper(self,root,res,level):
+        if not root:
+            return
+        if level==len(res):
+            res.append(root.val)
+        self.helper(root.right,res,level+1)
+        self.helper(root.left,res,level+1)
