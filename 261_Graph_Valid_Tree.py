2b3d08d4d2b7275356fc0b1e80974fc7eb719035
Maggie Ruimin Zhao <rz2390@columbia.edu>
2018-02-18 19:28:02 -0500

    Add files via upload

--- 261_Graph_Valid_Tree.py
+++ 261_Graph_Valid_Tree.py
@@ -1,23 +1,21 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def rightSideView(self, root):
+class Solution:
+    def validTree(self, n, edges):
         """
-        :type root: TreeNode
-        :rtype: List[int]
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: bool
         """
-        res=[]
-        self.helper(root,res,0)
-        return res
-    def helper(self,root,res,level):
-        if not root:
-            return
-        if level==len(res):
-            res.append(root.val)
-        self.helper(root.right,res,level+1)
-        self.helper(root.left,res,level+1)
+        if len(edges)!=n-1:
+            return False
+        neighbors={i:[] for i in range(n)}
+        for x,y in edges:
+            neighbors[x].append(y)
+            neighbors[y].append(x)
+        self.visit(0,neighbors)
+        return len(neighbors)==0
+    
+    def visit(self,i,neighbors):
+        temp=neighbors.pop(i)
+        for n in temp:
+            if n in neighbors:
+                self.visit(n,neighbors)
